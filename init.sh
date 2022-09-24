5b27fcdb73ed220e1e9d5ab12f484412e93db8b2
Amin <ak_1596@yahoo.com>
2022-09-24 16:08:47 +0330

    =Configs

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
-docker-compose run --rm ovpn ovpn_genconfig -u udp://YOURIP
+docker-compose run --rm ovpn ovpn_genconfig -u udp://${VPN_SERVER_IP}:${VPN_SERVER_PORT}
 docker-compose run --rm ovpn ovpn_initpki
